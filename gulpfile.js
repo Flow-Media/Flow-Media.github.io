30f0f9c41400949545534a789265404bd3b62ddd
Osama <iosamaify@gmail.com>
2020-05-27 14:42:59 +0200

    create a development server with gulp-connect

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,17 @@ const htmlmin = require("gulp-htmlmin");
 const sass = require("gulp-sass");
 const autoprefixer = require("gulp-autoprefixer");
 const sourcemaps = require("gulp-sourcemaps");
-const livereload = require("gulp-livereload");
+const connect = require("gulp-connect");
 const noop = require("gulp-noop");
 
-const isDev = process.env.NODE_ENV === "development";
+const { PORT, NODE_ENV } = process.env;
+
+const port = PORT ? +PORT : 5300;
+const isDev = NODE_ENV === "development";
+const livereload = true;
 
 function html() {
-  return gulp
+  gulp
     .src("src/**/*.html")
     .pipe(
       !isDev
@@ -22,11 +26,11 @@ function html() {
         : noop()
     )
     .pipe(gulp.dest("dist"))
-    .pipe(livereload());
+    .pipe(livereload ? connect.reload() : noop());
 }
 
 function css() {
-  return gulp
+  gulp
     .src("src/**/*.scss")
     .pipe(isDev ? sourcemaps.init() : noop())
     .pipe(
@@ -38,24 +42,32 @@ function css() {
     .pipe(autoprefixer({ cascade: false, grid: "autoplace" }))
     .pipe(isDev ? sourcemaps.write(".") : noop())
     .pipe(gulp.dest("dist"))
-    .pipe(livereload());
+    .pipe(livereload ? connect.reload() : noop());
 }
 
-function dev() {
-  livereload.listen({}, () => {
-    console.log("livereload is running...");
+function serve() {
+  connect.server({
+    root: "dist",
+    livereload,
+    port
   });
-
-  gulp.watch("src/styles/**/*.scss", css);
-  gulp.watch("src/**/*.html", html);
 }
 
 function build() {
-  return gulp.series(html, css);
+  gulp.series(html, css);
+}
+
+function dev() {
+  build();
+
+  serve();
+
+  gulp.watch("src/**/*.scss", css);
+  gulp.watch("src/**/*.html", html);
 }
 
 exports.html = html;
 exports.css = css;
-exports.dev = dev;
 exports.build = build;
+exports.dev = dev;
 exports.default = build;
