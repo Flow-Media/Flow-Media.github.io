b0968172720cd218f6475e97f1f30efe540f3302
Osama <iosamaify@gmail.com>
2020-05-28 07:59:32 +0200

    add gulp-imagemin

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,8 @@ const sass = require("gulp-sass");
 const autoprefixer = require("gulp-autoprefixer");
 const sourcemaps = require("gulp-sourcemaps");
 const connect = require("gulp-connect");
+const imagemin = require("gulp-imagemin");
 const noop = require("gulp-noop");
-
 const { PORT, NODE_ENV } = process.env;
 
 const port = PORT ? +PORT : 5300;
@@ -14,7 +14,7 @@ const livereload = true;
 
 function html(done) {
   gulp
-    .src("src/index.html")
+    .src("src/**/*.html")
     .pipe(
       !isDev
         ? htmlmin({
@@ -33,7 +33,7 @@ function html(done) {
 
 function scss(done) {
   gulp
-    .src("src/styles/style.scss")
+    .src("src/**/*.scss")
     .pipe(isDev ? sourcemaps.init() : noop())
     .pipe(
       sass({ outputStyle: isDev ? "expanded" : "compressed" }).on(
@@ -49,6 +49,28 @@ function scss(done) {
   done && done();
 }
 
+function image(done) {
+  gulp
+    .src([
+      "src/**/*.jpg",
+      "src/**/*.jpeg",
+      "src/**/*.png",
+      "src/**/*.gif",
+      "src/**/*.svg"
+    ])
+    .pipe(
+      imagemin([
+        imagemin.gifsicle({ interlaced: true }),
+        imagemin.mozjpeg({ quality: 75, progressive: true }),
+        imagemin.optipng({ optimizationLevel: 5 }),
+        imagemin.svgo({
+          plugins: [{ removeViewBox: false }, { cleanupIDs: false }]
+        })
+      ])
+    )
+    .pipe(gulp.dest("dist"));
+}
+
 function serve(done) {
   connect.server({
     root: "dist/",
@@ -60,18 +82,29 @@ function serve(done) {
 }
 
 function watch(done) {
-  gulp.watch("src/**/*.scss", scss);
   gulp.watch("src/**/*.html", html);
+  gulp.watch("src/**/*.scss", scss);
+  gulp.watch(
+    [
+      "src/**/*.jpg",
+      "src/**/*.jpeg",
+      "src/**/*.png",
+      "src/**/*.gif",
+      "src/**/*.svg"
+    ],
+    image
+  );
 
   done && done();
 }
 
-const build = gulp.series(html, scss);
+const build = gulp.series(html, scss, image);
 
 const dev = gulp.series(build, serve, watch);
 
 exports.html = html;
 exports.scss = scss;
+exports.image = image;
 exports.watch = watch;
 exports.build = build;
 exports.dev = dev;
